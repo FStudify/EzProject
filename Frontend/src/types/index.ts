cdaf67147689535d9c19e3f2d9a26e1639695d24
BaoKhanh04 <baokhanh652210@gmail.com>
2026-07-01 15:26:01 +0700

    fix2

--- Frontend/src/types/index.ts
+++ Frontend/src/types/index.ts
@@ -63,7 +63,7 @@ export interface Task {
   requestNote: string | null;
   comments: TaskComment[];
   commentsCount?: number;
-  hashtags: string[];
+  hashtags?: string[];
   createdAt: string;
   updatedAt?: string;
 }
